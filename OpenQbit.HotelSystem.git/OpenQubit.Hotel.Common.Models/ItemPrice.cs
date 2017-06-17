0fab688d199d7c6f402219dd5209e8683d49a4fb
Chameera Jayasinghe <chameerajayasinghe23@gmail.com>
2017-06-17 15:23:32 +0530

    missing files added

---
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.Common.Models/ItemPrice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQubit.Hotel.Common.Models
+{
+    public class ItemPrice
+    {
+        public int ItemPriceId { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime? ToDate { get; set; }
+        public double Price { get; set; }
+
+        public int ItemId { get; set; }
+
+        public virtual Item Item { get; set; }
+
+    }
+}
