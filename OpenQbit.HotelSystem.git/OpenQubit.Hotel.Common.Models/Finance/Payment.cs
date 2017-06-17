aaa64114c60397fa7427f6668f25974610c8cea0
ABC <ABC@DESKTOP-6135F2D>
2017-06-17 16:06:36 +0530

    Added models(RoomReservation,RoomReservationDetail,Tour & Payment) & Resource class

---
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.Common.Models/Finance/Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQubit.Hotel.Common.Models.Finance
+{
+    public class Payment
+    {
+        public int PaymentID { get; set; }
+
+        public int ReservationDetailID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public Double FullAmount { get; set; }
+    }
+}
---
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.Common.Models/Room/RoomReservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQubit.Hotel.Common.Models.Room
+{
+    public class RoomReservation
+    {
+        public int RoomReservationID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public DateTime CheckIn { get; set; }
+
+        public DateTime CheckOut { get; set; }
+
+        public virtual ICollection<RoomReservationDetail> RoomReservationDetail { get; set; }
+    }
+}
