44f41748a3790b625330d938f2e6d758a233bd44
ABC <ABC@DESKTOP-6135F2D>
2017-06-24 14:28:15 +0530

    HotelContext added

--- OpenQbit.HotelSystem.git/OpenQubit.Hotel.DAL.DataAccess/HotelContext.cs
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.DAL.DataAccess/HotelContext.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQubit.Hotel.Common.Models;
+using OpenQubit.Hotel.Common.Models.Finance;
 using OpenQubit.Hotel.Common.Models.Management;
-
+using OpenQubit.Hotel.Common.Models.Room;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace OpenQubit.Hotel.DAL.DataAccess
 {
-   public class HotelContext : DbContext
+    public class HotelContext : DbContext
     {
         public HotelContext() : base("HotelDB")
         {
@@ -25,6 +26,16 @@ namespace OpenQubit.Hotel.DAL.DataAccess
         public DbSet<OrderDetail> OrderDetail { get; set; }
         public DbSet<ItemPrice> ItemPrice { get; set; }
         public DbSet<JobType> JobType { get; set; }
+
+        public DbSet<Facility> Facility { get; set; }
+        public DbSet<Service> Service { get; set; }
+        public DbSet<RoomReservation> RoomReservation { get; set; }
+        public DbSet<RoomReservationDetail> RoomReservationDetail { get; set; }
+        public DbSet<Tour> Tour { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Resource> Resource { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
