340704c551bb1b9acae5feb07ed477813d39ef49
darshanap@openqubit.com <darshanatharaka15@gmail.com>
2017-07-07 19:12:51 +0530

    create bll

---
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.BLL.BusinessService/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQubit.Hotel.BLL.BusinessService
+{
+    class Class1
+    {
+        private void abc()
+        {
+            EmployeeManager em = new EmployeeManager();
+
+            Employee empl = em.Find(E => E.JobType.Title == "");
+
+        }
+
+    }
+}
--- OpenQbit.HotelSystem.git/OpenQubit.Hotel.BLL.BusinessService/EmployeeManager.cs
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.BLL.BusinessService/EmployeeManager.cs
@@ -4,11 +4,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQubit.Hotel.Common.Models;
+using System.Linq.Expressions;
+using OpenQubit.Hotel.DAL.DataAccess.Contracts;
+using OpenQubit.Hotel.Common.Utils.Logs;
 
 namespace OpenQubit.Hotel.BLL.BusinessService
 {
-    public class EmployeeManager :IEmployeeManager
+    public class EmployeeManager : IEmployeeManager
     {
+        private IRepository repository;
+        private ILogger logger;
+        public Employee Find(Expression<Func<Employee, bool>> predicate)
+        {
+            return repository.Find<Employee>(predicate);
+        }
 
+
+        public List<Employee> FindByEmployeeType(int employeetypeid)
+        {
+            return repository.FindList<Employee>(e => e.EmployeeId == employeetypeid);
+        }
+
+        public Employee FindById(int id)
+        {
+            return repository.Find<Employee>(e => e.EmployeeId == id);
+        }
+
+        public bool ModifyEmployee(Employee employee)
+        {
+            logger.logError("");
+            return repository.Update<Employee>(employee);
+        }
+
+        public bool RecordEmployee(Employee employee)
+        {
+            logger.logError("");
+            return repository.Create<Employee>(employee);
+        }
+
+        public bool RemoveEmployee(Employee employee)
+        {
+            logger.logError("");
+            return repository.Delete<Employee>(employee);
+        }
+        
     }
 }
