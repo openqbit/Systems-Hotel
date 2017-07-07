ee219b65403b122fd0f7b243011b8cf2fc08ab64
Chameera Jayasinghe <chameerajayasinghe23@gmail.com>
2017-07-07 13:52:21 +0530

    Add missing files

--- OpenQbit.HotelSystem.git/Openqubit.Hotel.Service.WebAPI/Properties/AssemblyInfo.cs
+++ OpenQbit.HotelSystem.git/Openqubit.Hotel.Service.WebAPI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ecebbfa-e160-45dd-89c6-55dcff9eb41a")]
+[assembly: Guid("7d24803b-488e-4b01-9bf5-686ac17927d5")]
 
 // Version information for an assembly consists of the following four values:
 //
