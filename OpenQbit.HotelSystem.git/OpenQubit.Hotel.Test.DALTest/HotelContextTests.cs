ed7c1be25f9d4f163176fd070d0fb24136c50ef4
maneeshmw <mwmaneesh@gmail.com>
2017-07-11 21:51:12 +0530

    Create Hotel DALTest Project

---
+++ OpenQbit.HotelSystem.git/OpenQubit.Hotel.Test.DALTest/HotelContextTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQubit.Hotel.DAL.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenQubit.Hotel.DAL.DataAccess.Tests
+{
+    [TestClass()]
+    public class HotelContextTests
+    {
+        [TestMethod()]
+        public void HotelContextTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
